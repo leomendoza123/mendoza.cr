034c588af43197266bcd9626618905cb987e7806
DESKTOP-BAL4HQR\leocr <leomendoza123@gmail.com>
2020-10-24 17:51:33 -0700

    Inits vue app with hero banner

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <HeroBanner />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeroBanner from "./components/HeroBanner.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HeroBanner,
+  },
+});
+</script>
+
+<style lang="scss">
+@import "./styles/styles.scss";
+body {
+  background: $base-color;
+  background-image: radial-gradient(rgba(256,256,256,0.4) 1px, transparent 0);
+  background-size: 40px 40px;
+  background-position: -19px -19px;
+
+  overflow-x: hidden;
+}
+h1 {
+  font-size: $header-1;
+  margin: 0;
+  font-weight: 400;
+}
+h2 {
+  font-size: $header-2;
+  font-weight: 400;
+  margin: 0;
+}
+
+h3 {
+  font-size: $header-3;
+  font-weight: 400;
+  margin: 0;
+}
+h4 {
+  font-size: $header-4;
+  font-weight: 400;
+  margin: 0;
+}
+p {
+  font-size: $paragraph;
+  font-weight: 400;
+  margin: 0;
+}
+
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $foreground-text;
+  position: relative;
+  max-width: 2560px;
+  margin: 0 auto;
+  margin-top: 80px;
+  padding: 30px;
+}
+</style>
---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1 class="slideInLeft">Build the future</h1>
+  <h1 class="slideInRight">Build web</h1>
+
+  <h2>Leonardo Mendoza</h2>
+  <h3>Software developer</h3>
+  <ul>
+    <li>More than 7 years working with web techonologies</li>
+    <li>Working expirience with Angular since it’s release in 2016</li>
+
+    <li>
+      Application performance, internationalizaiton and accesibility expert
+    </li>
+  </ul>
+
+  <Button :text="'HIRE ON UPWORK'" />
+  <PlanetIlustration class="planet"></PlanetIlustration>
+  <OrbitIlustration class="orbit" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+import PlanetIlustration from "./PlatnetIlustration.vue";
+import OrbitIlustration from "./OrbitIlustration.vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  components: {
+    Button,
+    PlanetIlustration,
+    OrbitIlustration,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../styles/styles.scss";
+@import "../styles/animations.scss";
+
+.orbit {
+  position: absolute;
+  top: 0;
+  width: 50%;
+  z-index: -50;
+  top: 0;
+  right: -200px;
+  opacity: 0.4;
+}
+
+.planet {
+  width: 947px;
+  position: absolute;
+  top: 40px;
+  right: 13%;
+  overflow: overlay;
+  z-index: -25;
+}
+
+button {
+  margin: 10px 0 10px 0;
+}
+
+ul {
+  padding-left: 25px;
+
+  // list-style-type: none;
+}
+</style>
